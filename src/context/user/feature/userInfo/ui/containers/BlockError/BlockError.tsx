0746b02aadb8e61a9548bf1ab75f5e88bf5b7531
Michael Vetkin <onvetka@gmail.com>
2024-01-24 17:27:18 +0300

    Deleted excess code

--- src/context/user/feature/userInfo/ui/containers/BlockError/BlockError.tsx
+++
@@ -1,9 +0,0 @@
-import { AppErrorViewState } from '../../../../../../../core/appError/ui/viewState/appErrorViewState';
-
-interface BlockErrorProps {
-  viewState: AppErrorViewState;
-}
-
-export const BlockError: React.FC<BlockErrorProps> = ({ viewState }) => {
-  return <div>{viewState.label}</div>;
-};
--- src/context/user/feature/userInfo/ui/viewState/ContactViewState.ts
+++
@@ -1,9 +0,0 @@
-export class ContactViewState {
-  label: string;
-  value: string;
-
-  constructor({ label, value }: { label: string; value: string }) {
-    this.label = label;
-    this.value = value;
-  }
-}
