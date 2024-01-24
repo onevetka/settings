c6c0cd0a9ff7cf3a32dd04b370c0f5eec24f28f7
Michael Vetkin <onvetka@gmail.com>
2024-01-24 15:17:03 +0300

    Added base components state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { ThemeProvider } from './components/ui/theme-provider';
-import { EmailNotificationForm } from './context/settings/feature/email-notification/ui/containers/email-notification-form/email-notification-form';
+import { EmailNotificationForm } from './context/settings/feature/email-notification/ui/containers/email-notification-form/EmailNotificationForm';
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
