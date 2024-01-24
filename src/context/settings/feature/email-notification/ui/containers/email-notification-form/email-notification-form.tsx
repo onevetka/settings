3b6ccf442dd157c24779fb752e74f98355bc59e8
Michael Vetkin <onvetka@gmail.com>
2024-01-24 15:06:45 +0300

    Added new button fixed layout

--- src/context/settings/feature/email-notification/ui/containers/email-notification-form/email-notification-form.tsx
+++ src/context/settings/feature/email-notification/ui/containers/email-notification-form/email-notification-form.tsx
@@ -6,18 +6,20 @@ export const EmailNotificationForm: React.FC = () => {
   return (
     <form onSubmit={() => null} className="w-full space-y-6">
       <div>
-        <h3 className="mb-4 text-lg font-medium">Email Notifications</h3>
+        <h3 className="mb-4 text-left text-lg font-medium">
+          Email Notifications
+        </h3>
         <div className="space-y-4">
           <div className=" flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
             <div className="space-y-0.5">
-              <div>Marketing emails</div>
+              <div className="text-left">Marketing emails</div>
               <div>Receive emails about new products, features, and more.</div>
             </div>
             <Switch checked={false} onCheckedChange={() => null} />
           </div>
           <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
             <div className="space-y-0.5">
-              <div>Security emails</div>
+              <div className="text-left">Security emails</div>
               <div>Receive emails about your account security.</div>
             </div>
             <Switch
@@ -29,7 +31,12 @@ export const EmailNotificationForm: React.FC = () => {
           </div>
         </div>
       </div>
-      <Button type="submit">Submit</Button>
+      <div className="flex justify-end gap-2">
+        <Button variant="secondary" type="submit">
+          Discard
+        </Button>
+        <Button type="submit">Save</Button>
+      </div>
     </form>
   );
 };
